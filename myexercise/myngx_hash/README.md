a2b6b31e3d4e9b5f9e08c7d78a22eff8e9fa81f4
lotluck <1219345363@qq.com>
2019-07-07 10:28:01 +0800

    2019-07-07 10:28:01 引用nginx的.o文件编写nginx哈希表的测试代码， 非常重要的能力

---
+++ myexercise/myngx_hash/README.md
@@ -0,0 +1,10 @@
+
+nginx-1.16.0
+====
+myngx_hash.c --- [调试练习ngx_hash.c的代码](https://wait.com)<br>
+----
+
+
+## 引用nginx的.o文件，编写nginx的测试代码真是个技术活，重点是解决各种依赖，这才是真本事
+
+## 同时也能熟悉nginx的hash代码，了解其精髓
--- myexercise/myngx_hash/myngx_hash.c
+++ myexercise/myngx_hash/myngx_hash.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include "ngx_config.h"
+#include "ngx_conf_file.h"
+#include "nginx.h"
+#include "ngx_core.h"
+#include "ngx_string.h"
+#include "ngx_palloc.h"
+#include "ngx_array.h"
+#include "ngx_hash.h"
+
+
+
+volatile ngx_cycle_t  *ngx_cycle;
+void ngx_log_error_core(ngx_uint_t level, ngx_log_t *log, ngx_err_t err, const char *fmt, ...)
+{
+}
+
 //gcc -m32 test_ngx_hash_elt_t.c
 //pragma 控制内存是否对齐
 
@@ -15,12 +31,21 @@ typedef struct {
      * 可以申请一段连续的空间
      */
     unsigned char    name[1];            
-} ngx_hash_elt_t;  
+} myngx_hash_elt_t;  
 
 //#pragma  pack()
 
 int main()
 {
-    printf("sizeof(ngx_hash_elt_t) = %d\n", sizeof(ngx_hash_elt_t));
+    printf("sizeof(myngx_hash_elt_t) = %lu\n", sizeof(myngx_hash_elt_t));
+ 
+    printf("sizeof(ngx_hash_elt_t) = %lu\n", sizeof(ngx_hash_elt_t));
+
     return 0;
 }
+
+
+
+
+
+
