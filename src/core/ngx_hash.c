a169e7e8db86052f43713da829905bca2024c9d8
vaynedu <1219345363@qq.com>
2019-08-08 18:24:57 +0800

    2019-08-08 18:24:57 增加 ngx_hash_wildcard_init and ngx_hash_find_wc_head注释

--- src/core/ngx_hash.c
+++ src/core/ngx_hash.c
@@ -20,12 +20,15 @@ ngx_hash_find(ngx_hash_t *hash, ngx_uint_t key, u_char *name, size_t len)
     ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, 0, "hf:\"%*s\"", len, name);
 #endif
 
+	// 这里计算该key会在哪一个桶中
     elt = hash->buckets[key % hash->size];
 
     if (elt == NULL) {
         return NULL;
     }
 
+
+	/*每个bucket的最后一个元素为void*, 所以while的终止条件就是elt->value == NULL */
     while (elt->value) {
         if (len != (size_t) elt->len) {
             goto next;
@@ -42,7 +45,14 @@ ngx_hash_find(ngx_hash_t *hash, ngx_uint_t key, u_char *name, size_t len)
 
     next:
         
-        // 这里的地址偏移到下一个ngx_hash_elt_t结构
+        /* 
+		 * 这里的地址偏移到下一个ngx_hash_elt_t结构
+		 *
+		 * 其实每个bucket就是一段bucket_size长度的连续内存空间
+		 *
+		 * 这里存放的都是冲突元素
+		 */
+		
         elt = (ngx_hash_elt_t *) ngx_align_ptr(&elt->name[0] + elt->len,
                                                sizeof(void *));
         continue;
@@ -51,6 +61,20 @@ ngx_hash_find(ngx_hash_t *hash, ngx_uint_t key, u_char *name, size_t len)
     return NULL;
 }
 
+/*
+ * 功能：在前置通配符哈希表中，查找key对应的value值
+ * 
+ * 参数: @name  关键字key
+ *       @len   关键字对应的长度
+ *
+ * return : 返回key(server name)对应的value， 一般这个value为用户配置
+ *
+ *
+ * 实现过程： 创建通配符哈希表时，采用递归的方式创建哈希表。而查询过程也是一样，递归查询
+ *            函数ngx_hash_find_wc_head用来在前置通配符哈希表中查找到key对应的value值
+ *
+ *
+ * */
 
 void *
 ngx_hash_find_wc_head(ngx_hash_wildcard_t *hwc, u_char *name, size_t len)
@@ -82,6 +106,16 @@ ngx_hash_find_wc_head(ngx_hash_wildcard_t *hwc, u_char *name, size_t len)
     ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, 0, "key:\"%ui\"", key);
 #endif
 
+
+	/*
+	 * 参考： https://blog.csdn.net/apelife/article/details/53106140 
+	 *
+	 * 1. 查找这个单词是否在哈希表中存在
+	 * 2. 如果在哈希表中查找到key，则返回value值
+	 * 3. 这里返回的value值有可能是最终关键字*.baidu.com.cn对应的value
+	 * 4. 也可能是指向下一个子哈希表的指针
+	 *
+	 * */
     value = ngx_hash_find(&hwc->hash, key, &name[n], len - n);
 
 #if 0
@@ -269,6 +303,11 @@ ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
     }
 
     for (n = 0; n < nelts; n++) {
+		/*
+		 * 每个桶至少能放下一个元素
+		 *
+		 * sizeof(void *) 是桶的结束位置
+		 * */
         if (hinit->bucket_size < NGX_HASH_ELT_SIZE(&names[n]) + sizeof(void *))
         {
             ngx_log_error(NGX_LOG_EMERG, hinit->pool->log, 0,
@@ -284,11 +323,30 @@ ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
         return NGX_ERROR;
     }
 
+	/*
+	 * sizeof(void *) 是桶的结束标志
+	 *
+	 * hinit->bucket_size - sizeof(void *)才是桶使用的实际大小空间
+	 *
+	 * */
     bucket_size = hinit->bucket_size - sizeof(void *);
+
+    /*
+	 * bucket_size / (2 * sizeof(void *) 计算一个桶内，最大含有多少个元素
+	 *
+	 * nelts / (bucket_size / (2 * sizeof(void *))) 这么多元素总共需要多少个桶， nelts是实际的元素个数
+	 *
+	 * start ? start : 1  至少保证有一个通
+	 * */
+
 
     start = nelts / (bucket_size / (2 * sizeof(void *)));
     start = start ? start : 1;
 
+	/*
+	 * 这里是一个经验值,如果元素个数很多,初始值就把桶的数量设置多一点
+	 *
+	 * */
     if (hinit->max_size > 10000 && nelts && hinit->max_size / nelts < 100) {
         start = hinit->max_size - 1000;
     }
@@ -303,6 +361,11 @@ ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
             }
 
             key = names[n].key_hash % size;
+
+			/*
+			 * test[key] 放着hash冲突的元素，不断变长
+			 *
+			 * */
             test[key] = (u_short) (test[key] + NGX_HASH_ELT_SIZE(&names[n]));
 
 #if 0
@@ -311,6 +374,10 @@ ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
                           size, key, test[key], &names[n].key);
 #endif
 
+			/*
+			 * 如果冲突的元素大小 超过  bucket_size， 说明桶的个数设置太少，继续for循环
+			 *
+			 * */
             if (test[key] > (u_short) bucket_size) {
                 goto next;
             }
@@ -319,6 +386,15 @@ ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
         goto found;
 
     next:
+
+		/*
+		 * 没有最好的算法，只有最适合的算法
+		 *
+		 * 虽然说goto大家很反对，代码跳来跳去，但是呢，小部分使用，让逻辑更清晰简单，容易实现
+		 *
+		 * 我就绝对nginx对goto的使用恰到好处
+		 *
+		 * */
 
         continue;
     }
@@ -333,7 +409,11 @@ ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
                   hinit->name, hinit->bucket_size, hinit->name);
 
 found:
-
+   
+	/*
+	 * 到此已经找到合适的bucket数量，即为size
+	 * 重新初始化test数组元素，初始值为一个指针大小
+	 * */
     for (i = 0; i < size; i++) {
         test[i] = sizeof(void *);
     }
@@ -349,6 +429,11 @@ found:
 
     len = 0;
 
+	/*
+	 * 为了提高访问速度，每个bucket的大小会和ngx_cacheline_size 对齐
+	 *
+	 * len 计算bucket所需要的总长度
+	 * */
     for (i = 0; i < size; i++) {
         if (test[i] == sizeof(void *)) {
             continue;
@@ -384,14 +469,21 @@ found:
         return NGX_ERROR;
     }
 
+	/*地址空间对齐可以提升访问速度*/
     elts = ngx_align_ptr(elts, ngx_cacheline_size);
 
+	/*
+	 * 将bucket地址数组和对应bucket关联
+	 *
+	 * */
     for (i = 0; i < size; i++) {
         if (test[i] == sizeof(void *)) {
             continue;
         }
 
         buckets[i] = (ngx_hash_elt_t *) elts;
+
+		/* test[i] 记录着预算过的每个桶的带下*/
         elts += test[i];
     }
 
@@ -399,6 +491,12 @@ found:
         test[i] = 0;
     }
 
+	/*
+	 *  将name的元素存储在bucket中
+	 *
+	 *  test数组用于保存对应bucket已经使用的字节数
+	 * 
+	 * */
     for (n = 0; n < nelts; n++) {
         if (names[n].key.data == NULL) {
             continue;
@@ -415,6 +513,12 @@ found:
         test[key] = (u_short) (test[key] + NGX_HASH_ELT_SIZE(&names[n]));
     }
 
+	/*
+	 * 每个bucket的最后一个元素设置为NULL
+	 *
+	 * ngx_hash_find函数中，就以elt->value为while终止条件 
+	 *
+	 * */
     for (i = 0; i < size; i++) {
         if (buckets[i] == NULL) {
             continue;
@@ -464,11 +568,19 @@ found:
 }
 
 
-/* 支持通配符哈希表*/
 
 /* 
+ * 通配符哈希表初始化
+ *
  * ngx_hash_wildcard_init其实构造了一个多级hash表
  *
+ * 为什么会这么设计？nginx这么设计有什么好处？
+ *
+ *
+ * 参数：
+ * hinit ：初始化结构体指针
+ * names : 预加入哈希表数组
+ * nelts : 数组大小
  */
 ngx_int_t
 ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
@@ -481,6 +593,8 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
     ngx_hash_init_t       h;
     ngx_hash_wildcard_t  *wdc;
 
+
+	/* 初始化临时动态数组curr_names,curr_names是存放当前关键字的数组 */
     if (ngx_array_init(&curr_names, hinit->temp_pool, nelts,
                        sizeof(ngx_hash_key_t))
         != NGX_OK)
@@ -488,6 +602,8 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
         return NGX_ERROR;
     }
 
+
+    /* 初始化临时动态数组next_names,next_names是存放关键字去掉后剩余关键字 */
     if (ngx_array_init(&next_names, hinit->temp_pool, nelts,
                        sizeof(ngx_hash_key_t))
         != NGX_OK)
@@ -495,6 +611,22 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
         return NGX_ERROR;
     }
 
+	/*
+	 * 参考： https://ivanzz1001.github.io/records/post/nginx/2018/04/06/nginx-source_part40_3
+	 *
+	 * 这里for循环时，n可能不是每次+1， 这是因为在处理到有相同前缀的元素时，会构造子hash，后面这些元素都交由子Hash来处理
+	 * 
+	 * 1) 寻找到names[n]中的第一个'.'字符（例如上面cn.com.baidu.)
+	 *
+	 * 2) 将寻找到的元素放入curr_names数组中(这里查找的元素为cn)
+	 *
+	 * 3) 假如查找到的元素长度不等于names[n]的总长度, 则初始化next_names数组(例如，上面len(cn) != len(cn.com.baidu))
+	 *
+	 * 4) 从names[n+1]开始，判断后续是否有与上面查找到的元素同前缀的names， 如果有则加到next_names数组中
+	 *
+	 * 5)  假如next_names数组长度不为0，则递归调用ngx_hash_wildcard_init()函数，构建子级Hash 
+	 *
+	 * */
     for (n = 0; n < nelts; n = i) {
 
 #if 0
@@ -504,6 +636,7 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
 
         dot = 0;
 
+		//以.作为字段的分隔符
         for (len = 0; len < names[n].key.len; len++) {
             if (names[n].key.data[len] == '.') {
                 dot = 1;
@@ -516,8 +649,18 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
             return NGX_ERROR;
         }
 
+
+		//将上面获取的字段作为当前hash的key
         name->key.len = len;
         name->key.data = names[n].key.data;
+		/*
+		 * hinit->key(name->key.data, name->key.len) 调用设置的函数指针
+		 *
+		 * 这主要是用大写呢，还是小写计算hash值
+		 *
+		 * 背景：有些场景需要忽略大小（客户要求域名）
+		 *
+		 * */
         name->key_hash = hinit->key(name->key.data, name->key.len);
         name->value = names[n].value;
 
@@ -584,6 +727,10 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
             h = *hinit;
             h.hash = NULL;
 
+			/*
+			 * 剩余的字段创造一个递归的hash
+			 *
+			 * */
             if (ngx_hash_wildcard_init(&h, (ngx_hash_key_t *) next_names.elts,
                                        next_names.nelts)
                 != NGX_OK)
@@ -593,12 +740,13 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
 
             wdc = (ngx_hash_wildcard_t *) h.hash;
 
+			/* 全匹配的Hash值在wdc->value处 */
             if (names[n].key.len == len) {
                 wdc->value = names[n].value;
             }
 
             /*
-             *  巧妙代码标记 : wonderful_code
+             *  巧妙代码标记 : wonderful_code_flag
              *  
              *  使用了指针的低位携带额外信息，节省了内存
              *
@@ -606,6 +754,13 @@ ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
              *
              */
 
+			/* name->value = (void *) ((uintptr_t) wdc | (dot ? 3 : 2)) 当前value值指向新的hash表
+			 *
+			 * 这里dot来区分该元素是否匹配到了最末尾
+			 *
+			 * 这里2表示当前已经到了一个匹配的结尾，即已经获得了一个全量匹配，不需要再匹配下去了
+			 *
+			 */
             name->value = (void *) ((uintptr_t) wdc | (dot ? 3 : 2));
 
         } else if (dot) {
@@ -732,6 +887,20 @@ ngx_hash_keys_array_init(ngx_hash_keys_arrays_t *ha, ngx_uint_t type)
     return NGX_OK;
 }
 
+
+/*
+ * 1. 判断元素添加到哪一种类的Hash表中(完全匹配，前缀匹配，后缀匹配)
+ *     skip=1: 表示.example.com这种通配类型
+ *     skip=2: 表示*.example.com这种通配类型
+ *     skip=0: 表示www.example.*这种通配类型，此时将last值进行调整last-=2，即减去后面.*两个字符的长度
+ * 
+ * 2. 对通配字符串进行相应的格式转换 .example.com会被转换成 com.example
+ *
+ * 3. 在wildcard hash中检查是否有冲突
+ *
+ * 4. 添加转换后的通配字符串到数组中
+ *
+ * */
 
 ngx_int_t
 ngx_hash_add_key(ngx_hash_keys_arrays_t *ha, ngx_str_t *key, void *value,
